234d4bd4c3fe12a4af82b6d68ec6f45dd6178ec8
cklxx <1293822641@qq.com>
2025-10-22 13:38:40 +0800

    Disable tokenizer parallelism warning

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,14 @@
-"""
-MiniGPT 源代码包
-"""
+"""Project package initialisation for :mod:`mini-llm`."""
+
+from __future__ import annotations
+
+import os
+
+# NOTE: HuggingFace tokenizers spawn worker pools that do not interact well with
+# the fork semantics used in some of our multiprocessing utilities.  When a
+# tokenizer instance is created before a process fork happens, the default
+# behaviour is to emit a noisy warning about parallelism being disabled.  We
+# avoid the warning – and the associated risk of a deadlock – by explicitly
+# disabling tokenizers parallelism globally.  The environment variable must be
+# set before any tokenizer code is imported, so we do it at package import time.
+os.environ.setdefault("TOKENIZERS_PARALLELISM", "false")
